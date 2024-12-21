9026cfa2c7c2f1c22a44e614a494e45506b0c0dd
Leorev01 <revrennaleo@gmail.com>
2024-12-21 19:16:57 +0000

    Adds logo to website and bounce animation to button

--- components/SalesButton.tsx
+++ components/SalesButton.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const SalesButton = () => {
   return (
-    <button className='bg-[#EF233C] text-black text-2xl p-4 mt-5 rounded-lg flex flex-col items-center transition-transform transform animate-bounce'>
+    <button className='bg-[#EF233C] text-black font-bold text-2xl p-4 mt-5 rounded-lg flex flex-col items-center transition-transform transform animate-bounce'>
         SCHEDULE A FREE CONSULTATION
-        <span className='text-lg'>20% OFF Christmas Sale</span>
+        <span className='text-lg font-normal'>20% OFF Christmas Sale</span>
     </button>
   );
 }
